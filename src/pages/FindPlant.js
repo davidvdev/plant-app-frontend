696f0fd96aa787d302db1226507d48ca0b1da043
davidvdev <david@davidvdev.com>
2021-08-25 18:46:14 -0500

    myplants displaying on search page. working on filtering

--- src/pages/FindPlant.js
+++ src/pages/FindPlant.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 
 const FindPlant = (props) => {
 
     //state to store the search term
-    const [searchTerm, setSearchTerm] =useState("")
+    const [searchTerm, setSearchTerm] = useState("")
     //state to store the filtered plants
     const [results, setResults] = useState(props.myPlants)
 
@@ -26,6 +27,18 @@ const FindPlant = (props) => {
                 onChange={handleChange}
             />
         </div>
+        {results.map(item => {
+            return (
+                    <Link to="/current-plant">
+                        <div className="plant-container" onClick={() => props.selectPlant(item)}>
+                            <img className="plant-img" src={item.plantType.picture}/>
+                            <h3 className="plant-name">{item.nickname}</h3>
+                            <h4 className="plant-type">{item.plantType.botName}</h4>
+                        </div>
+                    </Link>
+                    )
+            }   
+            )}
     </>
 };
 
